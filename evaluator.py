b60ae0092adc334cb505a457727ac8035a35b168
Silvio Mejia <silviomejiagarcia@gmail.com>
2025-06-29 14:43:22 -0600

    agregando sesgo

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,5 @@
 import chess
+import random
 
 class Nodo:
     def __init__(self, tablero, movimiento=None, padre=None):
@@ -9,10 +10,11 @@ class Nodo:
         self.valor = None
 
 class ArbolMinimax:
-    def __init__(self, tablero, maxDepth, color):
+    def __init__(self, tablero, maxDepth, color, probabilidad_error=0.0):
         self.raiz = Nodo(tablero.copy())
         self.maxDepth = maxDepth
-        self.color = color  # Color que queremos que gane
+        self.color = color
+        self.probabilidad_error = probabilidad_error  # Sesgo: 0.3 = 30% de error
 
     def construir_arbol(self, nodo, profundidad, es_maximizador):
         if profundidad == 0 or nodo.tablero.is_game_over():
@@ -26,9 +28,7 @@ class ArbolMinimax:
                 nuevo_tablero.push(movimiento)
                 hijo = Nodo(nuevo_tablero, movimiento, nodo)
                 nodo.hijos.append(hijo)
-                eval_hijo = self.construir_arbol(
-                    hijo, profundidad - 1, nuevo_tablero.turn == self.color
-                )
+                eval_hijo = self.construir_arbol(hijo, profundidad - 1, False)
                 max_eval = max(max_eval, eval_hijo)
             nodo.valor = max_eval
             return max_eval
@@ -39,28 +39,27 @@ class ArbolMinimax:
                 nuevo_tablero.push(movimiento)
                 hijo = Nodo(nuevo_tablero, movimiento, nodo)
                 nodo.hijos.append(hijo)
-                eval_hijo = self.construir_arbol(
-                    hijo, profundidad - 1, nuevo_tablero.turn == self.color
-                )
+                eval_hijo = self.construir_arbol(hijo, profundidad - 1, True)
                 min_eval = min(min_eval, eval_hijo)
             nodo.valor = min_eval
             return min_eval
 
     def obtener_mejor_movimiento(self):
-        mejor_movimiento = None
-        mejor_valor = float('-inf') if self.color == chess.WHITE else float('inf')
+        hijos_validos = [h for h in self.raiz.hijos if h.valor is not None]
+        if not hijos_validos:
+            return None
 
-        for hijo in self.raiz.hijos:
-            if self.color == chess.WHITE:
-                if hijo.valor is not None and hijo.valor > mejor_valor:
-                    mejor_valor = hijo.valor
-                    mejor_movimiento = hijo.movimiento
-            else:
-                if hijo.valor is not None and hijo.valor < mejor_valor:
-                    mejor_valor = hijo.valor
-                    mejor_movimiento = hijo.movimiento
+        reverse = self.color == chess.WHITE
+        hijos_ordenados = sorted(hijos_validos, key=lambda h: h.valor, reverse=reverse)
 
-        return mejor_movimiento
+        if self.color == chess.WHITE or random.random() > self.probabilidad_error:
+            return hijos_ordenados[0].movimiento
+
+        if len(hijos_ordenados) > 1:
+            indice_random = random.randint(1, len(hijos_ordenados) - 1)
+            return hijos_ordenados[indice_random].movimiento
+        else:
+            return hijos_ordenados[0].movimiento
 
     def evaluar_tablero(self, tablero):
         piece_values = {
@@ -81,5 +80,29 @@ class ArbolMinimax:
                     score += valor
                 else:
                     score -= valor
+
+        # Control del centro
+        casillas_centro = [chess.D4, chess.E4, chess.D5, chess.E5]
+        for casilla in casillas_centro:
+            atacantes_color = len(tablero.attackers(self.color, casilla))
+            atacantes_oponente = len(tablero.attackers(not self.color, casilla))
+            score += -.5 * (atacantes_color - atacantes_oponente)
+
+        # Desarrollo de piezas
+        piezas_inicio = [chess.B1, chess.G1, chess.C1, chess.F1] if self.color == chess.WHITE else [chess.B8, chess.G8, chess.C8, chess.F8]
+        for pieza in piezas_inicio:
+            if tablero.piece_at(pieza) is None:
+                score += 0.3
+
+        # Penalización por caballos en la orilla
+        casillas_orilla = [
+            chess.A1, chess.H1, chess.A8, chess.H8,
+            chess.A2, chess.H2, chess.A7, chess.H7,
+            chess.A3, chess.H3, chess.A6, chess.H6
+        ]
+        for casilla in casillas_orilla:
+            pieza = tablero.piece_at(casilla)
+            if pieza and pieza.piece_type == chess.KNIGHT and pieza.color == self.color:
+                score -= 0.5
 
         return score
--- logic.py
+++ logic.py
@@ -8,24 +8,20 @@ class JuegoAjedrez:
         self.movimientos_legales = []
         self.movimientos_realizados = []
         self.se_mueve = False
-        self.movimiento_sugerido = None  # Corrección: estaba como "movimientos_sugerido"
+        self.movimiento_sugerido = None
 
     # si la casilla origen es None, significa que no se ha seleccionado una pieza
     def seleccionar_casilla(self, fila, columna):
         casilla = chess.square(columna, 7 - fila)
         if self.casilla_origen is None:
             pieza = self.tablero.piece_at(casilla)
-            if pieza and pieza.color == self.tablero.turn:
-
-                # Se guarda la casilla como origen
+            if pieza and pieza.color == self.tablero.turn: 
                 self.casilla_origen = casilla
 
                 # se guardan los legal moves para ponerlos de manera visual al seleccionar una pieza
                 for movimiento in self.tablero.legal_moves:
                     if movimiento.from_square == casilla:
                         self.movimientos_legales.append(movimiento.to_square)
-
-        # si no es None significa que ya hay casilla de origen
         else:
             movimiento = chess.Move(self.casilla_origen, casilla)
 
@@ -35,9 +31,8 @@ class JuegoAjedrez:
                 return
 
             pieza_origen = self.tablero.piece_at(self.casilla_origen)
-
             es_promocion = (
-                pieza_origen.piece_type == chess.PAWN and
+                pieza_origen.piece_type == chess.PAWN and 
                 (chess.square_rank(casilla) == 0 or chess.square_rank(casilla) == 7)
             )
 
@@ -50,9 +45,7 @@ class JuegoAjedrez:
                 self.tablero.push(movimiento)
                 self.se_mueve = True
                 self.movimiento_sugerido = None
-
-            # si el movimiento es legal, se pushea el movimiento
-            # se limpia la lista de legal moves
+                
             self.movimientos_legales = []
 
             if self.se_mueve:
@@ -70,22 +63,18 @@ class JuegoAjedrez:
     def es_empate(self):
         return self.tablero.is_stalemate()
 
-    # buscar cómo implementarlo en el main
     def coronar_pieza(self, casilla):
-        if self.tablero.piece_type_at(casilla) == chess.PAWN and (
-            chess.square_rank(casilla) == 0 or chess.square_rank(casilla) == 7
-        ):
+        if self.tablero.piece_type_at(casilla) == chess.PAWN and (chess.square_rank(casilla) == 0 or chess.square_rank(casilla) == 7):
             self.tablero.set_piece_at(casilla, chess.Piece(chess.QUEEN, self.tablero.color_at(casilla)))
 
     def deshacer_ultimo_movimiento(self):
-        self.movimientos_realizados.pop()
-        self.tablero.move_stack.pop()
+        if self.movimientos_realizados and self.tablero.move_stack:
+            self.movimientos_realizados.pop()
+            self.tablero.pop()
 
-# Función para recomendar el mejor movimiento usando Minimax
 def recomendar_movimiento(juego):
     color_turno = juego.tablero.turn
-    arbol = ArbolMinimax(juego.tablero, 3, color_turno)
-    es_maximizador = (juego.tablero.turn == arbol.color)
-    arbol.construir_arbol(arbol.raiz, 3, es_maximizador)
-    mejor_movimiento = arbol.obtener_mejor_movimiento()
-    return mejor_movimiento
+    prob_error = 0.3 if color_turno == chess.BLACK else 0.0
+    arbol = ArbolMinimax(juego.tablero, 3, color_turno, probabilidad_error=prob_error)
+    arbol.construir_arbol(arbol.raiz, 3, True)
+    return arbol.obtener_mejor_movimiento()
